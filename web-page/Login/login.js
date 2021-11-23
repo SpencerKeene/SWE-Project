fdc560ae932354893c52dcdeb3589c788b702c20
nicolas korsunski <nick.kor23@gmail.com>
2021-11-23 18:30:43 -0500

    added a validation variable

--- web-page/Login/login.js
+++ web-page/Login/login.js
@@ -1,7 +1,7 @@
 
 var loginEmail = "";
 var loginPw    = "";
-
+var valid      = false;
 const loginForm = document.getElementById('loginForm'); 
 
 loginForm.addEventListener('submit', (e) => {
@@ -9,5 +9,11 @@ loginForm.addEventListener('submit', (e) => {
     loginEmail = document.getElementById('inputEmail').value;
     loginPw    = document.getElementById('inputPassword').value;
     //add validation and API
+    if(valid == true){
+        //allow and send user to their instance of calendar
+    }
+    else{
+        e.preventDefault();
+    }
     //add a way to load to the next page if information is valid
 });
--- web-page/signup/signup.js
+++ web-page/signup/signup.js
@@ -3,15 +3,20 @@ var fname = "";
 var lname = "";
 var email = "";
 var pw    = "";
-
+var valid = false;
 const signUpForm = document.getElementById('signupForm');
 
 signUpForm.addEventListener('submit', (e) => {
-    e.preventDefault();
     fname = document.getElementById('fname').value;
     lname = document.getElementById('lname').value;
     email = document.getElementById('inputEmail').value;
     pw    = document.getElementById('inputPassword').value;
     //add validation and API
+    if(valid == true){
+        //allow and send user to their instance of calendar
+    }
+    else{
+        e.preventDefault();
+    }
     //add a way to load to the next page if information is valid
 });
