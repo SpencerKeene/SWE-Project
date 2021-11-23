202a0f10c352f2554c49d5dfe5fd52e24b977ffe
SpencerKeene <spencerakeene@gmail.com>
2021-11-22 23:29:32 -0500

    Update main page
    - Add ability to change the calendar month
    - Add base for future features, such as creating event on click of calendar cell or select month on click of calendar title
    - Fix bug that produced 7 padding days after if month ended on a Saturday
    - Add some comments for readability
    - Update styles.css

--- web-page/script.js
+++ web-page/script.js
@@ -1,3 +1,4 @@
+// define variables
 const todaysDate = new Date();
 const calendarGrid = document.getElementById("calendar-grid");
 const months = [
@@ -14,34 +15,40 @@ const months = [
   "November",
   "December",
 ];
+let calendarYear = todaysDate.getFullYear();
+let calendarMonth = todaysDate.getMonth();
 
-function setupCalendar(yearOffset = 0, monthOffset = 0) {
-  console.log("todays date:", todaysDate);
+// define functions
+function setupCalendar() {
+  // define inner functions
+  function addEventOnDay() {}
 
-  const month = todaysDate.getMonth() + monthOffset;
-  const year = todaysDate.getFullYear() + yearOffset;
-  const date = new Date(year, month);
-
-  document.getElementById("calendar-month").innerText = months[month];
-  document.getElementById("calendar-year").innerText = year;
-
-  // generate variables for adding calendar cells
-  const isThisMonth =
-    date.getFullYear() === todaysDate.getFullYear() &&
-    date.getMonth() === todaysDate.getMonth();
-  const daysInMonth = new Date(year, month + 1, 0).getDate();
-  const daysInPrevMonth = new Date(year, month, 0).getDate();
-  const paddingDaysBefore = new Date(year, month, 1).getDay();
-  const paddingDaysAfter = 7 - new Date(year, month + 1, 1).getDay();
-
-  // add cells
   function addCell(day) {
-    const cell = document.createElement("div");
+    const cell = document.createElement("button");
     cell.classList.add("col", "calendar-cell");
     cell.innerText = day;
     calendarGrid.appendChild(cell);
     return cell;
   }
+
+  // clear calendar grid
+  calendarGrid.innerHTML = "";
+
+  // set calendar title
+  document.getElementById("calendar-title-month").innerText =
+    months[calendarMonth];
+  document.getElementById("calendar-title-year").innerText = calendarYear;
+
+  // generate variables for adding calendar cells
+  const isThisMonth =
+    calendarYear === todaysDate.getFullYear() &&
+    calendarMonth === todaysDate.getMonth();
+  const daysInMonth = new Date(calendarYear, calendarMonth + 1, 0).getDate();
+  const daysInPrevMonth = new Date(calendarYear, calendarMonth, 0).getDate();
+  const paddingDaysBefore = new Date(calendarYear, calendarMonth, 1).getDay();
+  const paddingDaysAfter = (7 - ((paddingDaysBefore + daysInMonth) % 7)) % 7;
+
+  // add cells
   for (
     let i = 1;
     i <= paddingDaysBefore + daysInMonth + paddingDaysAfter;
@@ -49,10 +56,13 @@ function setupCalendar(yearOffset = 0, monthOffset = 0) {
   ) {
     let isPaddingDay = false;
     let day = i - paddingDaysBefore;
+    // if cell is a padding day before
     if (i <= paddingDaysBefore) {
       day += daysInPrevMonth;
       isPaddingDay = true;
-    } else if (i > paddingDaysBefore + daysInMonth) {
+    }
+    // if cell is a padding day after
+    else if (i > paddingDaysBefore + daysInMonth) {
       day -= daysInMonth;
       isPaddingDay = true;
     }
@@ -64,11 +74,36 @@ function setupCalendar(yearOffset = 0, monthOffset = 0) {
   }
 }
 
-function loadEvents(yearOffset, monthOffset) {}
+function loadEvents() {}
 
-function loadCalendar(yearOffset = 0, monthOffset = 0) {
-  setupCalendar(yearOffset, monthOffset);
+function loadCalendar() {
+  setupCalendar();
   loadEvents();
 }
 
+function monthLeft() {
+  if (calendarMonth === 0) {
+    calendarMonth = 11;
+    calendarYear--;
+  } else {
+    calendarMonth--;
+  }
+  loadCalendar();
+}
+
+function monthRight() {
+  if (calendarMonth === 11) {
+    calendarMonth = 0;
+    calendarYear++;
+  } else {
+    calendarMonth++;
+  }
+  loadCalendar();
+}
+
+// add methods to DOM elements
+document.getElementById("calendar-title-left-btn").onclick = monthLeft;
+document.getElementById("calendar-title-right-btn").onclick = monthRight;
+
+// load calendar on page load
 loadCalendar();
