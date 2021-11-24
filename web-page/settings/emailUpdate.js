e0fa7a4cd788c2efb6388cce93b0d48d1cf68eba
nicolas korsunski <nick.kor23@gmail.com>
2021-11-23 20:56:06 -0500

    edited js

--- web-page/settings/emailUpdate.js
+++ web-page/settings/emailUpdate.js
@@ -5,7 +5,8 @@ const update = document.getElementById('emailBtn');
 update.addEventListener('click', (e) => {
     newEmail = document.getElementById('changedEmail').value;
     if(true){//email does not exist
-
+        //update to server
+        alert("Email has been updated");
     }
     else{
         e.preventDefault();
