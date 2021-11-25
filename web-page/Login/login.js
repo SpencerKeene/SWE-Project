d7c2452d886a591dae48c06378c50706e5faba1c
nicolas korsunski <nick.kor23@gmail.com>
2021-11-24 21:06:33 -0500

    reformated

--- web-page/Login/login.js
+++ web-page/Login/login.js
@@ -1,19 +1,17 @@
-
 var loginEmail = "";
-var loginPw    = "";
-var valid      = false;
-const loginForm = document.getElementById('loginForm'); 
+var loginPw = "";
+var valid = false;
+const loginForm = document.getElementById("loginForm");
 
-loginForm.addEventListener('submit', (e) => {
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  loginEmail = document.getElementById("inputEmail").value;
+  loginPw = document.getElementById("inputPassword").value;
+  //add validation and API
+  if (valid == true) {
+    //allow and send user to their instance of calendar
+  } else {
     e.preventDefault();
-    loginEmail = document.getElementById('inputEmail').value;
-    loginPw    = document.getElementById('inputPassword').value;
-    //add validation and API
-    if(valid == true){
-        //allow and send user to their instance of calendar
-    }
-    else{
-        e.preventDefault();
-    }
-    //add a way to load to the next page if information is valid
+  }
+  //add a way to load to the next page if information is valid
 });
