d7fe78316494bb15b224faa17d18f9261ba1a184
SpencerKeene <spencerakeene@gmail.com>
2021-11-25 10:15:06 -0500

    Revert "Refactor code"
    This reverts commit d2a3c9d9f12decb7816e93545fdba693e3ca9a87.

--- web-page/script.js
+++ web-page/script.js
@@ -75,114 +75,112 @@ function setupCalendar() {
   ) {
     const cell = addCell();
 
-    let day = undefined;
-    let month = calendarMonth;
-    let year = calendarYear;
-
     // if cell is a padding day before
     if (i <= paddingDaysBefore) {
-      day = i - paddingDaysBefore + daysInPrevMonth;
-      month = calendarMonth === 0 ? 11 : calendarMonth - 1;
-      year = calendarMonth === 0 ? calendarYear - 1 : calendarYear;
+      const day = i - paddingDaysBefore + daysInPrevMonth;
 
+      cell.setAttribute("day", day);
+      cell.setAttribute("month", calendarMonth === 0 ? 11 : calendarMonth - 1);
+      cell.setAttribute(
+        "year",
+        calendarMonth === 0 ? calendarYear - 1 : calendarYear
+      );
+
+      cell.innerText = day;
       cell.classList.add("padding-cell");
     }
     // if cell is a padding day after
     else if (i > paddingDaysBefore + daysInMonth) {
-      day = i - paddingDaysBefore - daysInMonth;
-      month = calendarMonth === 11 ? 0 : calendarMonth + 1;
-      year = calendarMonth === 11 ? calendarYear + 1 : calendarYear;
+      const day = i - paddingDaysBefore - daysInMonth;
 
+      cell.setAttribute("day", day);
+      cell.setAttribute("month", calendarMonth === 11 ? 0 : calendarMonth + 1);
+      cell.setAttribute(
+        "year",
+        calendarMonth === 11 ? calendarYear + 1 : calendarYear
+      );
+
+      cell.innerText = day;
       cell.classList.add("padding-cell");
     }
     // is not a padding day
     else {
-      day = i - paddingDaysBefore;
-      month = calendarMonth;
-      year = calendarYear;
+      const day = i - paddingDaysBefore;
 
+      cell.innerText = day;
+      cell.innerText += "\n" + display(day, calendarMonth, calendarYear);
+      cell.setAttribute("day", day);
+      cell.setAttribute("month", calendarMonth);
+      cell.setAttribute("year", calendarYear);
       if (isThisMonth && day === todaysDate.getDate()) cell.id = "today";
     }
-
-    cell.setAttribute("day", day);
-    cell.setAttribute("month", month);
-    cell.setAttribute("year", year);
-    cell.innerText = day;
-    cell.innerText += "\n" + displayEvents(day, month, year);
   }
 }
 //nicks start
 const events = {
   id: 1,
-  name: "example name 1 27th",
-  startDate: "2021 11 27 12:30",
-  endDate: "2021 11 27 13:00",
+  name:"example name 1 27th",
+  startDate:"2021 11 27 12:30",
+  endDate:"2021 11 27 13:00"
 };
 const events1 = {
   id: 2,
-  name: "example name 2 28th",
-  startDate: "2021 11 28 14:30",
-  endDate: "2021 11 28 16:00",
+  name:"example name 2 28th",
+  startDate:"2021 11 28 14:30",
+  endDate:"2021 11 28 16:00"
 };
 const events2 = {
   id: 3,
-  name: "example name 3 26th",
-  startDate: "2021 11 26 10:30",
-  endDate: "2021 11 26 11:00",
+  name:"example name 3 26th",
+  startDate:"2021 11 26 10:30",
+  endDate:"2021 11 26 11:00"
 };
 const eventsList = [];
 eventsList[events.id] = events;
 eventsList[events1.id] = events1;
 eventsList[events2.id] = events2;
 
-function sortArray() {
-  eventsList.sort((event1, event2) => {
+function sortArray(){
+    eventsList.sort((event1,event2)=>{
     var date1 = parseDate(event1);
     var date2 = parseDate(event2);
-    if (date1 > date2) {
-      return 1;
-    } else if (date1 < date2) {
-      return -1;
-    } else {
-      return 0;
+    if(date1 > date2){
+        return 1;
+    }
+    else if(date1 < date2){
+        return -1
+    }
+    else{
+        return 0;
     }
   });
 }
 
-function displayEvents(day, month, year) {
-  sortArray();
-  var eventName = "";
-  eventsList.forEach((event) => {
-    var eventDate = parseDate(event);
-    if (
-      eventDate.getDate() === day &&
-      eventDate.getMonth() === month &&
-      eventDate.getFullYear() === year
-    ) {
-      eventName += event.name + "\n";
-    }
-  });
-
-  return eventName;
+function display(day, month, year){
+    sortArray();
+    var eventName = "";
+    eventsList.forEach(event=>{
+      var eventDate = parseDate(event);
+      if(eventDate.getDate() === day && eventDate.getMonth() === month && eventDate.getFullYear() === year){
+        eventName += event.name + "\n";
+      }
+    });
+    
+    return eventName;
+  
 }
 
-function parseDate(event) {
+function parseDate(event){
   var startDateComponents = event.startDate.split(" ");
-  var startYear = startDateComponents[0];
-  var startMonth = startDateComponents[1];
-  var startDay = startDateComponents[2];
-  var startDateTimeComponents = startDateComponents[3].split(":");
-  var startHour = startDateTimeComponents[0];
-  var startMinutes = startDateTimeComponents[1];
-  var meridiemStart;
-  var eventDate = new Date(
-    startYear,
-    startMonth - 1,
-    startDay,
-    startHour,
-    startMinutes
-  );
-  return eventDate;
+      var startYear = startDateComponents[0];
+      var startMonth = startDateComponents[1];
+      var startDay = startDateComponents[2];
+      var startDateTimeComponents = startDateComponents[3].split(":");
+      var startHour = startDateTimeComponents[0];
+      var startMinutes = startDateTimeComponents[1];
+      var meridiemStart;
+      var eventDate = new Date(startYear, startMonth-1, startDay, startHour, startMinutes);
+      return eventDate;
 }
 //nicks end
 
